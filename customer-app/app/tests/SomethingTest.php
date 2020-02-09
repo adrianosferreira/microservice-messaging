ed40c10fb32fac07b3d77c951518a718feab2034
Adriano Ferreira <adriano.f@onthegosystems.com>
2020-02-09 18:18:33 -0200

    Fixing CI

---
+++ customer-app/app/tests/SomethingTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SomethingTest extends TestCase
+{
+    public function testSomethingNice() {
+        $this->assertTrue(true);
+    }
+}
